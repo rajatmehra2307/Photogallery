1a5622acd770543ad4336ec5c33f743e25c39106
Rajat Mehra <rajatm2307@gmail.com>
2023-03-27 23:55:12 -0400

    Change description

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def getPhotoDetails(photo_id):
     photo['PhotoID'] = photo_id
     photo['CreationTime'] = "April 30, 2022"
     photo['Title'] = "New Bird"
-    photo['Description'] = "Yellow bird sitting on a tree"
+    photo['Description'] = "Yellow bird sitting?"
     photo['Tags'] = "bird,tree,nature,forest"
     photo['URL'] = "/media/bird.jpg" 
     return photo
